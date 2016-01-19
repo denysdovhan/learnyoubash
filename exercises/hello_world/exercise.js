351a2f8e7aa41c7083d09e610edf9f725d6f464d
Denys Dovhan <email@denysdovhan.com>
2016-01-19 21:27:01 +0200

    Add workshopper-adventure.

---
+++ exercises/hello_world/exercise.js
@@ -0,0 +1,16 @@
+const exercise = require('workshopper-adventure')();
+// const filecheck = require('workshopper-exercise/filecheck');
+// const execute = require('workshopper-exercise/execute');
+// const comparestdout = require('workshopper-exercise/comparestdout');
+
+
+// checks that the submission file actually exists
+// exercise = filecheck(exercise)
+
+// execute the solution and submission in parallel with spawn()
+// exercise = execute(exercise)
+
+// compare stdout of solution and submission
+// exercise = comparestdout(exercise)
+
+module.exports = exercise
--- learnyoubash.js
+++ learnyoubash.js
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+const workshopper = require('workshopper-adventure');
+const path        = require('path');
+
+learnyoubash = workshopper({
+    name        : 'learnyoubash'
+  , title       : 'Learn Bash'
+  , subtitle    : 'Bash is awesome!'
+  , appDir      : __dirname
+  , languages   : ['en']
+  , exerciseDir : path.join(__dirname, "exercises")
+  , defaultLang : 'en'
+  , header      : require('workshopper-adventure/default/header')
+  , footer      : require('workshopper-adventure/default/footer')
+});
+
+learnyoubash.addAll([
+  "HELLO WORLD"
+]);
+
+module.exports = learnyoubash;
