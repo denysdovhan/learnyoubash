1c58b5cb1c884351bdb9577520cdfa71313cbc77
Martin Heidegger <martin.heidegger@gmail.com>
2016-01-20 12:00:04 +0900

    Moved title to i18n.json because its more open to translation there :)

--- learnyoubash.js
+++ learnyoubash.js
@@ -1,9 +1,7 @@
 const workshopper = require('workshopper-adventure');
 
 learnyoubash = workshopper({
-    title       : 'Learn Bash'
-  , subtitle    : 'Bash is awesome!'
-  , appDir      : __dirname
+    appDir      : __dirname
   , languages   : ['en']
   , header      : require('workshopper-adventure/default/header')
   , footer      : require('workshopper-adventure/default/footer')
