192766fbdf81ba993907c5ab0762761da5c61b8f
Denys Dovhan <email@denysdovhan.com>
2016-02-03 00:03:44 +0200

    Add execution and comparison of solution.bash.

--- utils/problem.js
+++ utils/problem.js
@@ -1,34 +1,48 @@
 const path = require('path');
 const execute = require('./execute');
+const diff = require('./diff');
 
 "use strict";
 
-module.exports = dirname => ({
+module.exports = (dirname) => ({
 
   init: function (workshopper) {
-    this.problem = { file: path.join(dirname, `problem.{lang}.md`) };
+    this.problem =
+      { file: path.join(dirname, `problem.{lang}.md`) };
+    this.solution =
+      { file: path.join(dirname, `solution`, `solution.{lang}.md`) };
+    this.solutionPath =
+      path.resolve(dirname, `solution`, `solution.bash`);
   },
 
   verify: function (args, done) {
-    execute(args, true, (err, stdio, stdout, stderr, code) => {
+    execute(args, false, (err, stdio, stdout, stderr, code) => {
       if (err) { return done(err, false); }
       if (stderr.toString() !== '') {
-        console.error(`Bash Syntax Error:`);
-        console.error(stderr.toString());
+        process.stdout.write(`Bash Syntax Error:`);
+        process.stdout.write(stderr.toString());
         return done(false);
       }
-      console.log(stdio.toString());
-      done(true);
+
+      args[0] = this.solutionPath;
+      execute(args, false, (_err, _stdio, _stdout, _stderr, _code) => {
+        if (_err) { return done(_err, false); }
+        if (stdout.toString() !== _stdout.toString()) {
+          process.stdout.write(
+            diff(stdout.toString(), _stdout.toString())
+          );
+          return done(false);
+        }
+        done(true);
+      });
     });
   },
 
   run: function (args, done) {
     execute(args, true, (err, stdio, stdout, stderr, code) => {
-      if (err) {
-        done(err, false);
-      }
-      console.log(stdio.toString());
-      done()
+      if (err) { return done(err, false); }
+      done();
     });
   }
+
 });
