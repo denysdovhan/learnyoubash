f955a9b4dd073df08032b71f1b68fc797ae3e96c
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-07-05 15:36:47 +0100

    Fix dead zone for positional parameter
    When providing either `0` or `24` as a positional parameter the script "throws" the error echo:
    ```
    localhorst:solution BK$ ./solution.bash 0
    Error!
    localhorst:solution BK$ ./solution.bash 24
    Error!
    ```
    
    This commit includes `0` in the range in order to adhere to a proper 24h format.

--- exercises/if_conditional_statement/solution/solution.bash
+++ exercises/if_conditional_statement/solution/solution.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $1 -gt 0 && $1 -lt 12 ]]; then
+if [[ $1 -ge 0 && $1 -lt 12 ]]; then
   echo "Good morning!"
 elif [[ $1 -ge 12 && $1 -lt 18 ]]; then
   echo "Good afternoon!"
