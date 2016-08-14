31889a2b69d60478b67354f34a2b0c124fb99704
Denys Dovhan <email@denysdovhan.com>
2016-08-14 11:33:24 +0300

    Change footer file. Fix #17.

--- learnyoubash.js
+++ learnyoubash.js
@@ -1,10 +1,13 @@
 const workshopper = require('workshopper-adventure');
+const path = require('path');
 
 learnyoubash = workshopper({
     appDir      : __dirname
   , languages   : ['en','uk']
   , header      : require('workshopper-adventure/default/header')
-  , footer      : require('workshopper-adventure/default/footer')
+  , footer      : [
+      { file: path.join(__dirname, 'i18n', 'footer', '{lang}.md') }
+  ]
 });
 
 learnyoubash.addAll([
